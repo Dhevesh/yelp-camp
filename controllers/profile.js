c9756774e281c992064fad67954729dcca536890
IMS\Dheveshp <dheveshp@gmail.com>
2019-09-03 11:49:18 +0100

    switch user profile logic to user model

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express();
 
-const users = require("../models/users")
+const user = require("../models/users")
 const userProfile = require("../models/user-profile");
 const isAuthUser = require("../controllers/user-auth");
 
 router.get("/:id", isAuthUser.isUserProfile, (req, res)=>{
-    userProfile.findOne({"userId.id": req.params.id}, (err, foundUser)=>{
+    user.findById(req.params.id, (err, foundUser)=>{
         if (!err){
             res.render("profile/show", {user: foundUser});
         }
@@ -19,10 +19,8 @@ router.get("/:id/new", isAuthUser.isLoggedIn, (req, res)=>{
 });
 
 router.post("/:id/new", isAuthUser.isLoggedIn, (req, res)=>{
-    userProfile.create(req.body.profile, (err, newUserProfile)=>{
+    user.findByIdAndUpdate(req.params.id, req.body.profile, (err, newUserProfile)=>{
         if (!err){
-            newUserProfile.userId.id = req.user._id;
-            newUserProfile.save();
             res.redirect("/campgrounds");
         } else{
             console.log(err);
@@ -34,7 +32,7 @@ router.post("/:id/new", isAuthUser.isLoggedIn, (req, res)=>{
 // UPDATE USER ROUTES
 
 router.get("/:id/edit", isAuthUser.isUserProfile, (req, res)=>{
-    userProfile.findOne({"userId.id": req.params.id}, (err, foundUser)=>{
+    user.findById(req.params.id, (err, foundUser)=>{
         if (!err){
             res.render("profile/edit", {user: foundUser});
         }
@@ -42,18 +40,10 @@ router.get("/:id/edit", isAuthUser.isUserProfile, (req, res)=>{
 });
 
 router.put("/:id/edit", isAuthUser.isUserProfile, (req, res)=>{
-    console.log(req.params.id);
-    userProfile.findOne({"userId.id": req.params.id}, (err, foundUser)=>{
+    user.findByIdAndUpdate(req.params.id, req.body.profile, (err, updatedUser)=>{
         if (!err){
-            userProfile.findOneAndUpdate({"userId.id": req.params.id}, req.body.profile, (err, updatedUser)=>{
-                if (!err){
-                    res.redirect("/user/" + req.user._id);
-                } else{
-                    console.log(err);
-                    res.redirect("back");
-                }
-            });
-        } else {
+            res.redirect("/user/" + req.user._id);
+        } else{
             console.log(err);
             res.redirect("back");
         }
